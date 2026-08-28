c61e422bd8ac6defb5024aa4a8d3bb2e367e47cf
jAcquire Kingdom <jacquire.kingdom@gmail.com>
2026-08-27 19:31:06 -0700

    Delete src directory

--- src/utils/uiHelpers.ts
+++
@@ -1,8 +0,0 @@
-// src/utils/uiHelpers.ts
-export const truncateName = (name: string, maxLength: number = 6): string => {
-  if (!name) return '';
-  if (name.length > maxLength) {
-    return name.substring(0, maxLength);
-  }
-  return name;
-};
